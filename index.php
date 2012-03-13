7aff088de83a825b593c81e2d86c62ca4eb50f94
Christopher David Yudichak <TheOddLinguist@gmail.com>
2012-03-12 23:43:37 -0400

    add static content for index

--- index.php
+++ index.php
@@ -5,7 +5,73 @@ require('./inc/config.php');
 include(ROOT.'/inc/header.php');
 ?>
 
-<!-- Put your content here -->
+<div class="more-projects"><a href="projects.php"><img src="images/more-projects.png" alt="" title="View Our Projects" /></a></div>
+
+<div class="content">
+<div class="banner">
+
+	<div style="position: relative;" class="slideshow">
+    <a href="projects.php" title="View our Projects"><img src="images/banners/banner1.jpg" alt="" /></a>
+    <a href="projects.php" title="View our Projects"><img src="images/banners/banner2.jpg" alt="" /></a>
+    <a href="projects.php" title="View our Projects"><img src="images/banners/banner4.jpg" alt="" /></a>
+	</div>
+
+
+</div>
+<div class="sidebar">
+<div class="overlap">
+
+<div class="readmoreimg"><a href="about.php"><img src="images/readmore.png" alt="Read More..." title="Read More &rarr;" /></a></div>
+
+<h2>About Us</h2>
+
+<p>Champion Builders, Inc. was founded in May of 1986 and since that time has grown to be one of the premier general contractors in the Eastern Region of Pennsylvania.</p>
+
+<p>Champion is a full-service general contractor and services include:</p>
+
+<ul>
+<li>Land Selection</li>
+<li>Permit Processing</li>
+<li>Architectural and Engineering Services</li>
+<li>General Construction</li>
+</ul>
+
+Champion's construction services also include:
+
+<ul>
+<li>New Construction</li>
+<li>Warehousing</li>
+<li>Healthcare Construction, Interior and Experior Renovations</li>
+<li>Renovations</li>
+</ul>
+
+<!--<div class="readmore"><a href="">Read More</a> &rarr;</div>-->
+
+</div>
+</div>
+<div class="banners" style="border-right: 1px solid #b5b5b5;">
+
+<a href="about.php"><img src="images/banners/aboutus.jpg" title="About Champion Builders, Inc." alt="" /></a>
+<a href="services.php"><img src="images/banners/services.jpg" title="View our List of Services" alt="" /></a>
+<a href="projects.php"><img src="images/banners/projects.jpg" title="View our Projects" alt="" /></a>
+<a href="quote.php"><img src="images/banners/quote.jpg" title="Get a Quote" alt="" /></a>
+<a href="kirby.php"><img src="images/banners/kirby.jpg" title="Kirby Building Systems Authorized Supplier" alt="" /></a>
+<a href="employment.php"><img src="images/banners/employment.jpg" title="Want to work with Champion?" alt="" /></a>
+
+</div>
+</div>
+
+<div class="bottom-spacer">
+<div class="testimonial">
+<div class="testimonial-bg">
+
+<big>&#8220;</big>Champion Builders, Inc., General Contractors, performed in a most successful and satisfactory manner on the institutional health care, elderly residential care, and commercial projects which they have constructed for our Clients. Without exception, Champion Builders would be considered as a qualified, recommended and preferred General Contractor for our future projects.<big>&#8221;</big>
+
+<span>Swendsen Engineering Inc.</span>
+
+</div>
+</div>
+</div>
 
 <?
 include(ROOT.'/inc/footer.php');
